7b725b82643f2a5980872c24432f3a24cc543f95
Andrew Carpenter <andrew@audioinhd.com>
2016-03-09 09:39:36 -0500

    Initial commit of module

---
+++ index.js
@@ -0,0 +1,21 @@
+function updateProps(oldVnode, vnode) {
+  var key, cur, old, elm = vnode.elm,
+      oldProps = oldVnode.data.props || {}, props = vnode.data.props || {};
+  for (key in oldProps) {
+    if (!props[key]) {
+      delete elm[key];
+    }
+  }
+  for (key in props) {
+    cur = props[key];
+    old = oldProps[key];
+    if (old !== cur && (key !== 'value' || elm[key] !== cur)) {
+      if (key !== 'type') elm[key] = cur;
+      else {
+        try { elm.type = cur; } catch (e) { delete elm.type; }
+      }
+    }
+  }
+}
+
+module.exports = {create: updateProps, update: updateProps};
